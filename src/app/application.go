8ef6864c41bf9352c770783ed17f3fbc7c1c37ee
Pamela Quirós Ureña <pamelaquirosu@hotmail.com>
2022-10-19 21:42:08 -0600

    Section 5: Creating OAuth API

---
+++ src/app/application.go
@@ -0,0 +1,21 @@
+package app
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/pamela-quiros/bookstore_oauth-api/src/domain/access_token"
+	"github.com/pamela-quiros/bookstore_oauth-api/src/http"
+	"github.com/pamela-quiros/bookstore_oauth-api/src/repository/db"
+)
+
+var (
+	router = gin.Default()
+)
+
+func StartApplication() {
+	atHandler := http.NewHandler(access_token.NewService(db.NewRepository()))
+
+	router.GET("/oauth/access_token/:access_token_id", atHandler.GetById)
+
+	router.Run(":8080")
+
+}
